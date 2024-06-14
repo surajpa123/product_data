83250f566490e8808ee8552884d905ce902abdd7
surajpa123 <pathaksuraj136@gmail.com>
2024-06-14 15:52:58 +0530

    added vercel dep

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; 
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+
+module.exports = server;
+
